23964b26bd500486ffdd092411d883d8977e9899
Vikash Saini <vikashsaini01@gmail.com>
2017-08-15 16:07:16 +0530

    Added base project TaskFirst
    Added base project TaskFirst

---
+++ src/main/java/com/cheers/taskfirst/TaskFirstApplication.java
@@ -0,0 +1,17 @@
+package com.cheers.taskfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author hp1
+ *
+ */
+@SpringBootApplication
+public class TaskFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFirstApplication.class, args);
+	}
+}
